c4b264e84f84fe965417a70a654b775731a53775
Crystaleee <crystale.1031@gmail.com>
2017-11-01 14:59:58 -0400

    update profile partly done

--- dva-mvn/src/main/webapp/js/custom/controllers/HomeController.js
+++ dva-mvn/src/main/webapp/js/custom/controllers/HomeController.js
@@ -16,11 +16,36 @@
 
         function loadCurrentUser() {
             $scope.user = $rootScope.globals.currentUser;
+
+            var user = $scope.user;
+            user.newUsername = user.username;
+            user.newAge = user.age;
+            user.newHeight = user.height;
+            user.newWeight = user.weight;
+            user.bf = user.height - user.weight;
         }
 
         // $scope.logout = function() {
         //     AuthenticationService.ClearCredentials();
         // }
+
+        $scope.$watch('b', function(newValue) {
+            $scope.a = newValue;
+        });
+
+        $scope.updateProfile = function() {
+            var user = $scope.user;
+            user.height = user.newHeight;
+            user.weight = user.newWeight;
+            user.age = user.newAge;
+            user.username = user.newUsername;
+
+            $scope.selection = "timeline";
+        }
+
+        $scope.changeSelection = function(select) {
+            $scope.selection = select;
+        }
     }
 
 })();
