3f681e89330aee1d64a2da9d96f21becd6c192ef
Crystaleee <crystale.1031@gmail.com>
2017-11-16 14:40:15 -0500

    add functions in UserService
    UpdateProfile  & LoadCurrentUser

--- dva-mvn/src/main/webapp/js/custom/services/UserService.js
+++ dva-mvn/src/main/webapp/js/custom/services/UserService.js
@@ -13,6 +13,8 @@
         service.CheckUsername = CheckUsername;
         service.CheckEmail = CheckEmail;
         service.Signup = Signup;
+        service.UpdateProfile = UpdateProfile;
+        service.LoadCurrentUser = LoadCurrentUser;
 
         return service;
 
@@ -29,11 +31,7 @@
                     callback(data);
                 },
                 error: function(XMLHttpRequest, textStatus, errorThrown) {
-                    alert(XMLHttpRequest.readyState +
-                        XMLHttpRequest.status +
-                        XMLHttpRequest.responseText);
-                    console.log("error");
-                    console.log(textStatus);
+                    handleError(XMLHttpRequest, textStatus, errorThrown);
                 }
             });
         }
@@ -50,32 +48,70 @@
                 success: function(data) {
                     callback(data);
                 },
-                error: function(XMLHttpRequest,
-                    textStatus,
-                    errorThrown) {
-                    alert(XMLHttpRequest.readyState +
-                        XMLHttpRequest.status +
-                        XMLHttpRequest.responseText);
-                    console
-                        .log("error");
-                    console
-                        .log(textStatus);
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    handleError(XMLHttpRequest, textStatus, errorThrown);
                 }
             });
         }
 
         function Signup(form, callback) {
             console.log(form.serializeArray());
+            // $http({
+            //     method: "POST",
+            //     url: '/dva-mvn/signUp/signUp.do',
+            //     data: form
+            //
+            // }).then(function mySuccess(response) {
+            //     console.log(response);
+            //     callback(response);
+            // }, function myError(response) {
+            //
+            // });
             $.ajax({
                 url: '/dva-mvn/signUp/signUp.do',
                 type: 'post',
                 dataType: 'text',
                 async: false,
-                data: form
-                    .serializeArray(),
+                data: form.serializeArray(),
                 success: function(data) {
                     callback(data);
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    handleError(XMLHttpRequest, textStatus, errorThrown);
                 }
+            });
+        }
+
+        function UpdateProfile(form, callback) {
+            console.log(form.serializeArray());
+            $.ajax({
+                type: "post",
+                url: "/dva-mvn/UserInformation/updateProfile.do",
+                dataType: "text",
+                async: false,
+                data: form.serializeArray(),
+                success: function(data) {
+                    callback(data);
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    handleError(XMLHttpRequest, textStatus, errorThrown);
+                }
+
+            });
+        }
+
+        function LoadCurrentUser(callback) {
+            $.ajax({
+                type: "GET",
+                url: "/dva-mvn/UserInformation/loadUser.do",
+                async: false,
+                success: function(data) {
+                    callback(data);
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    handleError(XMLHttpRequest, textStatus, errorThrown);
+                }
+
             });
         }
 
@@ -105,13 +141,12 @@
             return res.data;
         }
 
-        function handleError(error) {
-            return function() {
-                return {
-                    success: false,
-                    message: error
-                };
-            };
+        function handleError(XMLHttpRequest, textStatus, errorThrown) {
+            alert(XMLHttpRequest.readyState +
+                XMLHttpRequest.status +
+                XMLHttpRequest.responseText);
+            console.log("error");
+            console.log(textStatus);
         }
     }
 
