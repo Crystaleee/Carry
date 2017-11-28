82e1031f419a5fdcac85bf7771429c6cca528268
qysong95 <31904169+qysong95@users.noreply.github.com>
2017-11-27 22:22:35 -0500

    update four tables by Song

--- dva-mvn/src/main/java/edu/bupt/dmg/domain/Exercise.java
+++ dva-mvn/src/main/java/edu/bupt/dmg/domain/Exercise.java
@@ -18,127 +18,41 @@ import org.hibernate.validator.constraints.NotBlank;
  * Version: @version 0.0.1
  */
 public class Exercise implements Serializable {
-	@NotBlank(message = "ExerciseCategory should not be blank!")
-	@Pattern(regexp = "^[a-zA-Z]{1}([a-zA-Z0-9]|[._]){4,19}$", message = "Make sure your username meet the requirement!")
-	private String ExerciseCategory;
+	@NotBlank(message = "Exercise name should not be blank!")
+	@Pattern(regexp = "^[a-zA-Z]{1}([a-zA-Z0-9]|[._]){4,19}$", message = "Make sure your exercise name meet the requirement!")
+	private String ExName;
 
-	@NotBlank(message = "ExerciseName should not be blank!")
-	private String ExerciseName;
+	@NotBlank(message = "Exercise calorie should not be blank!")
+	private String Ex_cal;
 
-	@NotBlank(message = "Time should not be blank!")
-	private String Time;
-
-	@NotBlank(message = "Frequency should not be blank!")
-	private String Frequency;
-	@NotBlank(message = "Exercise_cal should not be blank!")
-	private String Exercise_cal;
-
-
-	
-	private boolean AccountNonLocked;
-
-	private boolean Enabled;
-	private String EnableCode;
-	private String ExpirationDate;
 	// 关联对象
-	private List<UploadFile> uploadFiles;
-	private List<Role> roles;
-	
-	public String getExerciseCategory() {
-		return ExerciseCategory;
-	}
-
-	public void setExerciseCategory(String userId) {
-		this.ExerciseCategory = ExerciseCategory;
-	}
-	
-	public String getExerciseName() {
-		return ExerciseName;
-	}
-
-	public void setExerciseName(String ExerciseName) {
-		this.ExerciseName = ExerciseName;
-	}
-
-	public String getTime() {
-		return Time;
-	}
-
-	public void setTime(String Time) {
-		this.Time = Time;
-	}
-	
-	public String getBFrequency() {
-		return Frequency;
-	}
-
-	public void setFrequency(String Frequency) {
-		this.Frequency = Frequency;
-	}
-	
-	public String getExercise_cal() {
-		return Exercise_cal;
-	}
-
-	public void setExercise_cal(String Exercise_cal) {
-		this.Exercise_cal = Exercise_cal;
-	}
-
-	
-	public boolean isAccountNonLocked() {
-		return AccountNonLocked;
-	}
-
-	public void setAccountNonLocked(boolean accountNonLocked) {
-		this.AccountNonLocked = accountNonLocked;
-	}
-
-	public boolean isEnabled() {
-		return Enabled;
-	}
-
-	public void setEnabled(boolean enabled) {
-		this.Enabled = enabled;
-	}
-
-	public List<UploadFile> getUploadFiles() {
-		return uploadFiles;
-	}
-
-	public void setUploadFiles(List<UploadFile> uploadFiles) {
-		this.uploadFiles = uploadFiles;
-	}
-
-	public List<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(List<Role> roles) {
-		this.roles = roles;
-	}
+//	private List<UploadFile> uploadFiles;
+//	private List<Role> roles;
+//	private String BirthDate;
+//	private String Height;
+//	private String Weight;
+//	private String BMI;
+//	private String BFR;
 
 	@Override
 	public String toString() {
-		return "Exercise [ExerciseCategory=" + ExerciseCategory + ", ExerciseName=" + ExerciseName + ", Time=" + Time + ", Frequency=" + Frequency
-				+ ", Exercise_cal=" + Exercise_cal +"]";
+		return "Exercise [Exercise=" + ExName + ", Exercise_cal=" + Ex_cal  + "]";
+	}
+	
+	public String getExName() {
+		return ExName;
 	}
 
-
-
-	public String getEnableCode() {
-		return EnableCode;
+	public void setExName(String exName) {
+		ExName = exName;
 	}
 
-	public void setEnableCode(String enableCode) {
-		this.EnableCode = enableCode;
+	public String getEx_cal() {
+		return Ex_cal;
 	}
 
-	public String getExpirationDate() {
-		return ExpirationDate;
-	}
-
-	public void setExpirationDate(String expirationDate) {
-		this.ExpirationDate = expirationDate;
+	public void setEx_cal(String ex_cal) {
+		Ex_cal = ex_cal;
 	}
 
 }
--- dva-mvn/src/main/java/edu/bupt/dmg/domain/Food.java
+++ dva-mvn/src/main/java/edu/bupt/dmg/domain/Food.java
@@ -18,118 +18,42 @@ import org.hibernate.validator.constraints.NotBlank;
  * Version: @version 0.0.1
  */
 public class Food implements Serializable {
-	@NotBlank(message = "FoodCategory should not be blank!")
-	@Pattern(regexp = "^[a-zA-Z]{1}([a-zA-Z0-9]|[._]){4,19}$", message = "Make sure your FoodCategory meet the requirement!")
-	private String FoodCategory;
+	@NotBlank(message = "Food name should not be blank!")
+	@Pattern(regexp = "^[a-zA-Z]{1}([a-zA-Z0-9]|[._]){4,19}$", message = "Make sure your food name meet the requirement!")
+	private String FoodName;
 
-	@NotBlank(message = "Foodname should not be blank!")
-	private String Foodname;
-
-	@NotBlank(message = "Food_weight should not be blank!")
-	private String Food_weight;
-
-	@NotBlank(message = "Food_cal should not be blank!")
+	@NotBlank(message = "Food calorie should not be blank!")
 	private String Food_cal;
 
-
-
-	private boolean AccountNonLocked;
-
-	private boolean Enabled;
-	private String EnableCode;
-	private String ExpirationDate;
 	// 关联对象
-	private List<UploadFile> uploadFiles;
-	private List<Role> roles;
+//	private List<UploadFile> uploadFiles;
+//	private List<Role> roles;
+//	private String BirthDate;
+//	private String Height;
+//	private String Weight;
+//	private String BMI;
+//	private String BFR;
 
-	public String getFoodCategory() {
-		return FoodCategory;
-	}
-
-	public void setFoodCategory(String FoodCategory) {
-		this.FoodCategory = FoodCategory;
-	}
-	
-	public String getFoodname() {
-		return Foodname;
+	@Override
+	public String toString() {
+		return "Food [FoodName=" + FoodName + ", Food_cal=" + Food_cal  + "]";
 	}
 
-	public void setFoodname(String Foodname) {
-		this.Foodname = Foodname;
+	public String getFoodName() {
+		return FoodName;
 	}
 
-	public String getFood_weight() {
-		return Food_weight;
+	public void setFoodName(String foodName) {
+		FoodName = foodName;
 	}
 
-	public void setFood_weight(String Food_weight) {
-		this.Food_weight = Food_weight;
-	}
-	
 	public String getFood_cal() {
 		return Food_cal;
 	}
 
-	public void setFood_cal(String Food_cal) {
-		this.Food_cal = Food_cal;
+	public void setFood_cal(String food_cal) {
+		Food_cal = food_cal;
 	}
+
 	
-	
-	public boolean isAccountNonLocked() {
-		return AccountNonLocked;
-	}
-
-	public void setAccountNonLocked(boolean accountNonLocked) {
-		this.AccountNonLocked = accountNonLocked;
-	}
-
-	public boolean isEnabled() {
-		return Enabled;
-	}
-
-	public void setEnabled(boolean enabled) {
-		this.Enabled = enabled;
-	}
-
-	public List<UploadFile> getUploadFiles() {
-		return uploadFiles;
-	}
-
-	public void setUploadFiles(List<UploadFile> uploadFiles) {
-		this.uploadFiles = uploadFiles;
-	}
-
-	public List<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(List<Role> roles) {
-		this.roles = roles;
-	}
-
-	@Override
-	public String toString() {
-		return "Food [FoodCategory=" + FoodCategory + ", Foodname=" + Foodname + ", Food_weight=" + Food_weight + ", Food_cal=" + Food_cal
-				+  "]";
-	}
-
-
-
-	public String getEnableCode() {
-		return EnableCode;
-	}
-
-	public void setEnableCode(String enableCode) {
-		this.EnableCode = enableCode;
-	}
-
-	public String getExpirationDate() {
-		return ExpirationDate;
-	}
-
-	public void setExpirationDate(String expirationDate) {
-		this.ExpirationDate = expirationDate;
-	}
-
 }
-
