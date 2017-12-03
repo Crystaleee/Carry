073a6620e5fd7096f127d049948a558f218c3ac7
qysong95 <31904169+qysong95@users.noreply.github.com>
2017-12-03 00:20:01 -0500

    improve FigureController.js

--- dva-mvn/src/main/webapp/js/custom/controllers/FigureController.js
+++ dva-mvn/src/main/webapp/js/custom/controllers/FigureController.js
@@ -1,19 +1,65 @@
 (function() {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('app')
-        .controller('FigureController', FigureController);
+	angular.module('app').controller('FigureController', FigureController);
 
-    FigureController.$inject = ['$location', '$scope', 'AuthenticationService'];
+	FigureController.$inject = [ '$location', '$scope', 'AuthenticationService' ];
 
-    function FigureController($location, $scope, AuthenticationService) {
-        //initilization function
-        (function initController() {
+	function FigureController($location, $scope, AuthenticationService) {
+		// initilization function
+		(function initController() {
 
-        })();
+		})();
+		
+		$scope.DrawFigure = function() {
+			
+		};
 
+		$scope.inlineOptions = {
+			minDate : new Date(),
+			showWeeks : false
+		};
 
-    }
+		$scope.dateOptions = {
+			formatYear : 'yy',
+			maxDate : new Date(),
+			minDate : new Date(),
+			startingDay : 1
+		};
+
+		$scope.toggleMin = function() {
+			$scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null
+					: new Date();
+			$scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+		};
+
+		$scope.toggleMin();
+
+		$scope.open1 = function() {
+			$scope.popup1.opened = true;
+		};
+
+		$scope.open2 = function() {
+			$scope.popup2.opened = true;
+		};
+
+		$scope.setDate = function(year, month, day) {
+			$scope.user.birthdayUpdate = new Date(year, month, day);
+		};
+
+		$scope.formats = [ 'dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy',
+				'shortDate' ];
+		$scope.format = $scope.formats[0];
+		$scope.altInputFormats = [ 'M!/d!/yyyy' ];
+
+		$scope.popup1 = {
+			opened : false
+		};
+
+		$scope.popup2 = {
+			opened : false
+		};
+
+	}
 
 })();
