668942f031c8a53d3d3355d5834b872f5ba9d8bf
wbhnjld <1083573822@qq.com>
2017-12-03 20:44:10 -0500

    123

--- dva-mvn/src/main/java/edu/bupt/dmg/action/UserAction.java
+++ dva-mvn/src/main/java/edu/bupt/dmg/action/UserAction.java
@@ -81,40 +81,44 @@ public class UserAction {
 				token.setRememberMe(rememberme);
 				subject.login(token);
 				logger.info("user " + user.getUserId() + " has logged in!");
-				
-				
-				ExerciseRecord exerciseRecord=new ExerciseRecord();
-        		    exerciseRecord.setDate("2017/07/09");
-                exerciseRecord.setExercise_category("run;walk");
-                exerciseRecord.setExercise_time("1;3");
-                exerciseRecord.setUserID(user.getUserId());
-                FoodRecord foodRecord = new FoodRecord();
-                foodRecord.setDate("2017/08/09");
-               foodRecord.setFood_category("apple;peach");
-               foodRecord.setFood_weight("1:00");
-               foodRecord.setUserID(user.getUserId());
-              System.out.println("....>");
-               fileService.createExeRec(exerciseRecord);
-              fileService.createFoodRec(foodRecord);
-				System.out.println("hehehe");
-				
-				
-				List<ExerciseRecord> exeList=fileService.findExeFilesByUserId(user.getUserId());
-				List<FoodRecord> foodList=fileService.findFoodFilesByUserId(user.getUserId());
-				for(int i=0;i<exeList.size()-1;i++){
-					System.out.println("exeid: "+exeList.get(i).getRecordID());
-					System.out.println("Date: "+exeList.get(i).getDate());
-					System.out.println("Exercise_category: "+exeList.get(i).getExercise_category());
-					System.out.println("Exercise_time: "+exeList.get(i).getExercise_time());
-					System.out.println("userid: "+exeList.get(i).getUserID()+"\n");
-				}
-				for(int i=0;i<foodList.size()-1;i++){
-					System.out.println("exeid: "+foodList.get(i).getRecordID());
-					System.out.println("Date: "+foodList.get(i).getDate());
-					System.out.println("Food_category: "+foodList.get(i).getFood_category());
-					System.out.println("foodweight: "+foodList.get(i).getFood_weight());
-					System.out.println("userid: "+foodList.get(i).getUserID()+"\n");
-				}
+//				
+//				
+//				ExerciseRecord exerciseRecord=new ExerciseRecord();
+//        		    exerciseRecord.setDate("2017/07/09");
+//                exerciseRecord.setExercise_category("run;walk");
+//                exerciseRecord.setExercise_time("1;3");
+//                exerciseRecord.setUserID(user.getUserId());
+//                FoodRecord foodRecord = new FoodRecord();
+//                foodRecord.setDate("2017/08/09");
+//               foodRecord.setFood_category("apple;peach");
+//               foodRecord.setFood_weight("1:00");
+//               foodRecord.setUserID(user.getUserId());
+//              System.out.println("....>");
+//               fileService.createExeRec(exerciseRecord);
+//              fileService.createFoodRec(foodRecord);
+//				System.out.println("hehehe");
+//				
+//				
+//				List<ExerciseRecord> exeList=fileService.findExeFilesByUserId(user.getUserId());
+//				List<FoodRecord> foodList=fileService.findFoodFilesByUserId(user.getUserId());
+//				System.out.println("333444333");
+//				
+//				
+//				
+//				for(int i=0;i<exeList.size()-1;i++){
+//					System.out.println("exeid: "+exeList.get(i).getRecordID());
+//					System.out.println("Date: "+exeList.get(i).getDate());
+//					System.out.println("Exercise_category: "+exeList.get(i).getExercise_category());
+//					System.out.println("Exercise_time: "+exeList.get(i).getExercise_time());
+//					System.out.println("userid: "+exeList.get(i).getUserID()+"\n");
+//				}
+//				for(int i=0;i<foodList.size()-1;i++){
+//					System.out.println("exeid: "+foodList.get(i).getRecordID());
+//					System.out.println("Date: "+foodList.get(i).getDate());
+//					System.out.println("Food_category: "+foodList.get(i).getFood_category());
+//					System.out.println("foodweight: "+foodList.get(i).getFood_weight());
+//					System.out.println("userid: "+foodList.get(i).getUserID()+"\n");
+//				}
 				return new ResultMessage(1);
 			} else {
 				return new ResultMessage(-1);
