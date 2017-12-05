5d932213571deca1fdef3026d99fdc6c2a6dc2ea
ruitongchai <rchai@ur.rochester.edu>
2017-12-04 22:35:38 -0500

    a little change

--- dva-mvn/src/main/java/edu/bupt/dmg/sqlhelper/DatabaseHelper.java
+++ dva-mvn/src/main/java/edu/bupt/dmg/sqlhelper/DatabaseHelper.java
@@ -64,8 +64,8 @@ public class DatabaseHelper {
             String table_user = " CREATE TABLE IF NOT EXISTS user ( UserId varchar(45) ,UserName varchar(45) , UserPwd varchar(45) DEFAULT NULL, BirthDate varchar(45) DEFAULT NULL, Height float DEFAULT NULL, Weight float DEFAULT NULL,  BMI float DEFAULT NULL, BFR float DEFAULT NULL, Sex varchar(45) DEFAULT NULL, Email varchar(45) DEFAULT NULL, AccountNonLocked bool DEFAULT '0', Enabled bool DEFAULT '0', EnableCode varchar(45) DEFAULT NULL, ExpirationDate varchar(45) DEFAULT NULL) ;";
             String table_ex_rec = "CREATE TABLE IF NOT EXISTS Ex_record (Cal varchar(20) NOT NULL, RecordID int auto_increment primary key, Exercise_time varchar(45) NOT NULL,	UserID varchar(45) NOT NULL, Date varchar(45) NOT NULL,Exercise_category varchar(50) NOT NULL) ;";
             String drop_ex="drop table if exists Exercise;";
-            String drop_ex_rec="drop table if exists Ex_record;";
-            String drop_food_rec="drop table if exists Food_record;";
+            //String drop_ex_rec="drop table if exists Ex_record;";
+            //String drop_food_rec="drop table if exists Food_record;";
             String drop_food="drop table if exists Food;";
             String insert_ex="insert into Exercise(ExName,Ex_cal) VALUES('Swimming','400'),('Basketball','500'),('Fitness','550'),('Soccer','588'),('Jogging','600'),('Bicycling','330'),('Rope skipping','800'),('Squash','1000'),('Tennis','425'),('Volleyball','294');";
             String insert_food="insert into Food(FoodName, Food_cal)VALUES('Egg','86'),('Pork','241'),('Steak','250'),('Chicken','239'),('Broccoli','33.7'),('Carrot','41.3'),('Mushroom','22.2'),('Apple','54'),('Banana','88.7'),('Watermelon','30.4'),('Coca','37.5');";
@@ -176,7 +176,7 @@ public class DatabaseHelper {
 
             stmt.executeUpdate(drop_food);
             stmt.executeUpdate(drop_ex);
-            stmt.executeUpdate(drop_ex_rec);
+            //stmt.executeUpdate(drop_ex_rec);
             stmt.executeUpdate(table_user);
             stmt.executeUpdate(table_food);
             stmt.executeUpdate(table_food_rec);
